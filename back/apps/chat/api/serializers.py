6239922aad55f7c5adefa11a789c0de4f0d34a30
EugeneSamsonov <samsonovevgenii231@gmail.com>
2026-04-18 14:27:33 +0300

    back: add and fix message API

--- back/apps/chat/api/serializers.py
+++ back/apps/chat/api/serializers.py
@@ -1,10 +1,19 @@
 from rest_framework import serializers
+from django.contrib.auth.models import User
 
 from ..models import Message
 
 
+class SenderSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["username"]  # , "first_name", "last_name", "email"]
+        read_only_fields = ["username"]  # , "first_name", "last_name", "email"]
+
+
 class MessageSerializer(serializers.ModelSerializer):
-    sender = serializers.HiddenField(default=serializers.CurrentUserDefault())
+    sender = SenderSerializer(read_only=True)
+
     class Meta:
         model = Message
         fields = "__all__"
@@ -12,8 +21,8 @@ class MessageSerializer(serializers.ModelSerializer):
             "id",
             "room",
             "sender",
-            "text",
-            "chat_type",
+            # "text",
+            # "chat_type",
             "turn_number",
             "is_ai",
             "timestamp",
--- back/apps/chat/api/views.py
+++ back/apps/chat/api/views.py
@@ -3,6 +3,8 @@ from rest_framework import viewsets
 from rest_framework.exceptions import PermissionDenied, ValidationError
 from rest_framework.permissions import IsAuthenticated
 
+from rest_framework.pagination import LimitOffsetPagination
+
 from drf_spectacular.utils import extend_schema
 
 from apps.rooms.models import Participant, Room
@@ -11,32 +13,54 @@ from .serializers import MessageSerializer
 from ..models import Message
 
 
-@extend_schema(tags=['Игровой чат'])
+class ChatMessagePagination(LimitOffsetPagination):
+    default_limit = 50  # Сколько сообщений грузить по умолчанию
+    max_limit = 100
+
+
+@extend_schema(tags=["Игровой чат"])
 class MessageViewSet(viewsets.ModelViewSet):
     serializer_class = MessageSerializer
     permission_classes = [IsAuthenticated]
+    pagination_class = ChatMessagePagination
 
     def get_queryset(self):
-        room = get_object_or_404(Room, id=self.request.query_params.get("room"))
+        room_id = self.request.query_params.get("room")
+        chat_type = self.request.query_params.get("chat_type")
+
+        if not room_id:
+            raise ValidationError(
+                {"detail": "Не указан ID комнаты (параметр ?room=...)"}
+            )
+
+        room = get_object_or_404(Room, id=room_id)
 
         qs = (
             Message.objects.filter(room=room)
-            .order_by("timestamp")
+            .order_by("-timestamp")
             .select_related("sender")
         )
 
         if Participant.objects.filter(user=self.request.user, room=room).exists():
             return (
-                qs.filter(chat_type=self.request.query_params.get("chat_type"))
-                if self.request.query_params.get("chat_type")
+                qs.filter(chat_type=chat_type)
+                if chat_type
                 else qs
             )
         else:
             raise PermissionDenied()
 
     def perform_create(self, serializer):
+        room_id = self.request.query_params.get("room")
+
+        if not room_id:
+            raise ValidationError(
+                {"detail": "Не указан ID комнаты (параметр ?room=...)"}
+            )
+        room = get_object_or_404(Room, id=room_id)
+
         if serializer.validated_data.get("chat_type") == "GAME":
-            room = get_object_or_404(Room, id=serializer.validated_data.get("room"))
+            room = serializer.validated_data.get("room")
             participant = Participant.objects.filter(user=self.request.user, room=room)
 
             if room.status == "DM_TURN" and participant.role != "DM":
@@ -47,7 +71,9 @@ class MessageViewSet(viewsets.ModelViewSet):
 
             room.ready_players.add(self.request.user)
 
-            serializer.save(sender=self.request.user, turn_number=room.turn_count)
+            serializer.save(
+                sender=self.request.user, turn_number=room.turn_count, room=room
+            )
 
         else:
-            serializer.save(sender=self.request.user)
+            serializer.save(sender=self.request.user, room=room)
